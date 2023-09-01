69c04484ca545e4d3000cda8e46708b1e4a5b433
gboff <78048757+gboff@users.noreply.github.com>
2023-09-01 16:29:18 +0530

    Update StudentServiceImpl.java
    Adding edit feature

--- lib/src/main/java/com/gb/studentDetails/serviceImpl/StudentServiceImpl.java
+++ lib/src/main/java/com/gb/studentDetails/serviceImpl/StudentServiceImpl.java
@@ -28,15 +28,69 @@ public class StudentServiceImpl implements StudentService {
 	
 	@Override 
 	public Student updateStudent(Student student) {
-		int id = student.getStudentId();
-		Optional<Student> cust = studentRepo.findById(id);
-		if(cust.isPresent())
+		String id = student.getRollNumber();
+		Student cust = studentRepo.getStudentByRollNumber(id);
+		if(cust!=null)
+		{
+			String name1 = student.getName();
+			String nameAct = cust.getName();
+			if(name1 == null)
+				student.setName(nameAct);
+			
+			String mobileNumber1 = student.getMobileNumber();
+			String mobileNumberAct = cust.getMobileNumber();
+			if(mobileNumber1 == null)
+				student.setMobileNumber(mobileNumberAct);
+			
+			String className1 = student.getClassName();
+			String classNameAct = cust.getClassName();
+			if(className1 == null)
+				student.setClassName(classNameAct);
+			
+			String dob1 = student.getDob();
+			String dobAct = cust.getDob();
+			if(dob1 == null)
+				student.setDob(dobAct);
+			
+			String aadhar1 = student.getAadhar();
+			String aadharAct = cust.getAadhar();
+			if(aadhar1 == null)
+				student.setAadhar(aadharAct);
+			
+			String address1 = student.getAddress();
+			String addressAct = cust.getAddress();
+			if(address1 == null)
+				student.setAddress(addressAct);
+			
+			String bloodGroup1 = student.getBloodGroup();
+			String bloodGroupAct = cust.getBloodGroup();
+			if(bloodGroup1 == null)
+				student.setBloodGroup(bloodGroupAct);
+			
+			String emis1 = student.getEmis();
+			String emisAct = cust.getEmis();
+			if(emis1 == null)
+				student.setEmis(emisAct);
+			
+			String fatherName1 = student.getFatherName();
+			String fatherNameAct = cust.getFatherName();
+			if(fatherName1 == null)
+				student.setFatherName(fatherNameAct);
+			
+			String motherName1 = student.getMotherName();
+			String motherNameAct = cust.getMotherName();
+			if(motherName1 == null)
+				student.setMotherName(motherNameAct);
+			studentRepo.delete(cust);
 			return studentRepo.save(student);
+			
+		}
 		else
 		{
 			throw new StudentNotFoundException("No Student Found To Update");
 		}
 	}
+	
 	
 	@Override
 	public Student deleteStudent(int studentId) {
