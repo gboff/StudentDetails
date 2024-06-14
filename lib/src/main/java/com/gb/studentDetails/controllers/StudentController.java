e08db6595cd8e1ec2a81dbecab779feedb063982
gboff <78048757+gboff@users.noreply.github.com>
2024-06-15 03:09:00 +0530

    Update StudentController.java
    Adding controller for getStudentById service

--- lib/src/main/java/com/gb/studentDetails/controllers/StudentController.java
+++ lib/src/main/java/com/gb/studentDetails/controllers/StudentController.java
@@ -55,4 +55,10 @@ public class StudentController {
 	{
 		return studentServ.showAllStudents();
 	}
+
+	@GetMapping("/getStudentById/{studentId}")
+	public Student getStudentById(@PathVariable int studentId)
+	{
+		return studentServ.getStudentById(studentId);
+	}
 }
